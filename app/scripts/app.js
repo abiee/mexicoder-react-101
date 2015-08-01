73e3a473d4ac3bc83aa68b2216c703a1106ef88f
Abiee Echamea Márquez <abiee@Abiees-MacBook-Pro.local>
2015-08-01 13:55:19 -0500

    Stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,7 @@
 var React = require('react');
+var HelloWorld = require('./components/HelloWorld.jsx');
 
-console.log('React loaded', React);
+React.render(
+  <HelloWorld />,
+  document.getElementById('main')
+);
---
+++ app/scripts/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return ( 
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = HelloWorld;
