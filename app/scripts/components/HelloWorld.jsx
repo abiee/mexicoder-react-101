f57a7be41f4f889cbe29ffd1f8a4215a93e50244
Abiee Echamea Márquez <abiee@Abiees-MacBook-Pro.local>
2015-08-01 14:24:51 -0500

    more stuff

--- app/scripts/components/HelloWorld.jsx
+++ app/scripts/components/HelloWorld.jsx
@@ -1,10 +1,21 @@
 var React = require('react');
 
 var HelloWorld = React.createClass({
+  getInitialState: function() {
+    return {name: 'Abiee'};
+  },
+
+  changeName: function() {
+    this.setState({name: 'World!'});
+  },
+
   render: function() {
     return ( 
       <div>
-        <h1>Hello world</h1>
+        <h1>Hello {this.state.name}</h1>
+        <button className="btn btn-primary" onClick={this.changeName}>
+          Click me!
+        </button>
       </div>
     );
   }
